1d404ff77ad338342bdce8e9deff6cf630403b92
“Kopeerde” <kevin.mendesg@gmail.com>
2025-11-05 14:41:10 +0100

    Modification du boot
    - Passage en mode proteger.
    - Ajout de structure pour la GDT en C (WIP).

--- NASM/boot.asm
+++ NASM/boot.asm
@@ -11,20 +11,33 @@ section .multiboot
     dd MBFLAGS
     dd CHECKSUM
 
+
 section .bss
     align 16
 stack_bottom:
     resb 16384            ; réserve 16 Ko pour la pile
 stack_top:
 
+
 section .text
     global _start
     extern kernel_main
 
+
 _start:
+    cli                    ; Désactive interruptions
+    lgdt [gdtr]
+    mov eax, cr0
+    or al, 1
+    mov cr0, eax           ; Passage en mode protege
+    jmp 08h:PModeMain
     mov esp, stack_top     ; Initialise la pile
     call kernel_main       ; Appel du noyau en C
-    cli                    ; Désactive interruptions
+
+
+PModeMain:
+; load DS, ES, FS, GS, SS, ESP
+
 
 .hang:
     hlt                    ; Stoppe le processeur en attendant interruption
